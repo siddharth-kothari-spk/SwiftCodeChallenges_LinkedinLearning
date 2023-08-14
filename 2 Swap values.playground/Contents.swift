56f01f2455be74381cf03ae4892284d45f592035
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-14 16:55:19 +0530

    optimized for both values being same

--- 2 Swap values.playground/Contents.swift
+++ 2 Swap values.playground/Contents.swift
@@ -2,11 +2,14 @@ import Foundation
 
 // my solution
 func swapValues<T: Comparable>( value1: inout T, value2: inout T) {
-    print("Original: value1 = \(value1), value2 = \(value2)")
+    guard value1 != value2 else {
+        // no swap needed if both are same
+        print("no swap needed")
+        return
+    }
     let temp = value1
     value1 = value2
     value2 = temp
-    print("After swap: value1 = \(value1), value2 = \(value2)")
 }
 
 //swapValues(value1: 10, value2: 15) // Error: Cannot pass immutable value as inout argument: literals are not mutable
@@ -14,10 +17,17 @@ func swapValues<T: Comparable>( value1: inout T, value2: inout T) {
 var valInt1 = 10
 var valInt2 = 15
 
+print("Original: value1 = \(valInt1), value2 = \(valInt2)")
 swapValues(value1: &valInt1, value2: &valInt2)
+print("After swap: value1 = \(valInt1), value2 = \(valInt2)")
 
 var valString1 = "Ten"
 var valString2 = "Fifteen"
 
+print("Original: value1 = \(valString1), value2 = \(valString2)")
 swapValues(value1: &valString1, value2: &valString2)
+print("After swap: value1 = \(valString1), value2 = \(valString2)")
 
+var one = 100.0
+var two = 100.0
+swapValues(value1: &one, value2: &two)
