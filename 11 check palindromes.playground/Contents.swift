7ec4b988a0f8d9237694b31f37a84a9deae60f42
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-15 18:00:03 +0530

    single character string

--- 11 check palindromes.playground/Contents.swift
+++ 11 check palindromes.playground/Contents.swift
@@ -2,9 +2,14 @@ import Foundation
 
 func palindromeCheck(_ string: String) -> Bool {
     
+    guard string.count > 1 else {
+        return true
+    }
     return string.caseInsensitiveCompare(String(string.reversed())) == .orderedSame
 }
 
 print(palindromeCheck("madam"))
 print(palindromeCheck("Hannah"))
 print(palindromeCheck("baaba"))
+
+print(palindromeCheck("A"))
