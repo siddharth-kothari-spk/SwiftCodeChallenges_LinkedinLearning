7d89fad6ea9099705f7a104f83f9515d1af9f64c
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-15 18:02:58 +0530

    filter for only alphabets

--- 11 check palindromes.playground/Contents.swift
+++ 11 check palindromes.playground/Contents.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 func palindromeCheck(_ string: String) -> Bool {
+    let string = string.filter{$0.isLetter}
     
     guard string.count > 1 else {
         return true
@@ -13,3 +14,4 @@ print(palindromeCheck("Hannah"))
 print(palindromeCheck("baaba"))
 
 print(palindromeCheck("A"))
+print(palindromeCheck("Amore, roma"))
