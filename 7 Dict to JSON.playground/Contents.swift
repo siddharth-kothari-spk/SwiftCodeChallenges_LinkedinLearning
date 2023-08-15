387305fbcca722be43e77212b4ffe63f9d5291da
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-15 12:26:35 +0530

    using JSONSerialization

--- 7 Dict to JSON.playground/Contents.swift
+++ 7 Dict to JSON.playground/Contents.swift
@@ -1,8 +1,16 @@
 import Foundation
 
-let colorsDict = ["red":"FF0000", "blue":"00FF00","green":"0000FF"]
+let colorsDict = ["red":"FF0000", "blue":"00FF00","green":"0000FF","black":"FFFFFF", "white":"000000"]
 
+// using JSONEncoder
 if let jsonData = try? JSONEncoder().encode(colorsDict) {
+    if let jsonString = String(data: jsonData, encoding: .utf8) {
+        print(jsonString)
+    }
+}
+
+// using JSONSerialization
+if let jsonData = try? JSONSerialization.data(withJSONObject: colorsDict) {
     if let jsonString = String(data: jsonData, encoding: .utf8) {
         print(jsonString)
     }
