c835fb0b195824b86f91ff728ceafa53fe78bc31
Siddharth Kothari <siddharth.kothari.spk@gmail.com>
2023-08-15 17:42:27 +0530

    one liner using compactMap

--- 10 generate random passwords.playground/Contents.swift
+++ 10 generate random passwords.playground/Contents.swift
@@ -3,13 +3,16 @@ import Foundation
 
 func generatePassword(_ length: Int) -> String {
     let passwordString = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()-=_+"
-    
+    /*
     var password : String = ""
     
     for _ in 1...length {
         password.append(passwordString.randomElement()!)
     }
     return password
+    */
+    
+    return String((0..<length).compactMap{ _ in passwordString.randomElement()})
 }
 
 print(generatePassword(5))
